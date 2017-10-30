d357c932d95296bafcaeb6c261052fd642d6043c
Rafal Biegacz <rafal.biegacz@intel.com>
2017-10-30 22:30:34 +0100

    small tunning in problem3 and problem2 running scripts
    small tunning in problem3 and problem2 running scripts

--- AIND-Planning-master/problem2.bat
+++ AIND-Planning-master/problem2.bat
@@ -1,9 +1,6 @@
 python run_search.py -p 2 -s 1
-python run_search.py -p 2 -s 2
 python run_search.py -p 2 -s 3
-python run_search.py -p 2 -s 4
 python run_search.py -p 2 -s 5
-python run_search.py -p 2 -s 6
 python run_search.py -p 2 -s 7
 python run_search.py -p 2 -s 8
 python run_search.py -p 2 -s 9
