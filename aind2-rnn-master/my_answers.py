0225031027db905b0e4e6df466acfa2668c128ba
Rafal Biegacz <biegacz.rafal@gmail.com>
2018-03-03 10:07:26 +0100

    final version of my_answers.py file

--- aind2-rnn-master/my_answers.py
+++ aind2-rnn-master/my_answers.py
@@ -14,13 +14,13 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
-    wwindow_size = window_size - 1
-    iter_max = ceil(len(series)/window_size)
-    for iter in range(0, iter_max+1):
-      s_start = iter*wwindow_size
-      s_end = s_start + window_size
-      X.append(series[s_start:s_end])
-      y.append(series[s_end])
+#    wwindow_size = window_size - 1
+#    iter_max = ceil(len(series)/window_size)
+#    for iter in range(0, iter_max+1):
+#      s_start = iter*wwindow_size
+#      s_end = s_start + window_size
+#      X.append(series[s_start:s_end])
+#      y.append(series[s_end])
 #      if s_end < len(series):
 #        X.append(series[s_start:s_end])
 #        y.append(series[s_end])
@@ -28,8 +28,8 @@ def window_transform_series(series, window_size):
 #        X.append(series[s_start:-1])
 #        y.append(series[-1])
 
-#    inputs = [ series[i : i + window_size] for i in range(0, len(series) - window_size, window_size) ]
-#    outputs = [ series[i + window_size] for i in range(0, len(series) - window_size, window_size) ]
+    X = [series[i: i + window_size] for i in range(0, len(series) - window_size, 1)]
+    y = [series[i + window_size] for i in range(0, len(series) - window_size, 1)]
 
     # reshape each
     X = np.asarray(X)
