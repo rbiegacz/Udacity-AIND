e1e3148b0b2b79d675bb4f42e81322e209d221a1
Rafal Biegacz <biegacz.rafal@gmail.com>
2018-06-16 03:49:30 +0200

    deep_rnn_model(...) implemented

--- AIND-VUI-Capstone-master/sample_models.py
+++ AIND-VUI-Capstone-master/sample_models.py
@@ -98,9 +98,17 @@ def deep_rnn_model(input_dim, units, recur_layers, output_dim=29):
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
     # TODO: Add recurrent layers, each with batch normalization
-    ...
+    input = input_data
+    # add as many layers as recur_layers defines
+    for layer in range(recur_layers):
+        rnn_name = 'rnn_%d' % layer
+        bn_rnn_name = 'bn_' + rnn_name
+        rnn = GRU(units, activation='relu', return_sequences=True, implementation=2, name=rnn_name)(input)
+        input = BatchNormalization(name=bn_rnn_name)(rnn)
+
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = ...
+    time_dense = TimeDistributed(Dense(output_dim))(input)
+
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
@@ -115,9 +123,9 @@ def bidirectional_rnn_model(input_dim, units, output_dim=29):
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
     # TODO: Add bidirectional recurrent layer
-    bidir_rnn = ...
+    bidir_rnn = Bidirectional(GRU(units, return_sequences=True, name="bidirectional", implementation=2))(input_data)
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = ...
+    time_dense = TimeDistributed(Dense(output_dim))(bidir_rnn)
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
